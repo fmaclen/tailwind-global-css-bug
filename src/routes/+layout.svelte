8687a852a7eab194f7486fcb8cb83ae0d5163dc5
Fernando Maclen <fmaclen@gmail.com>
2024-11-16 15:22:05 -0500

    add reproduction

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,3 +4,16 @@
 </script>
 
 {@render children()}
+
+<div class="markdown">
+	{@html `<pre>
+		<code>Background should be blue, not red</code>
+	</pre>`}
+</div>
+
+<style lang="postcss">
+	.markdown :global(pre) {
+		@apply bg-red-500;
+		@apply dark:bg-blue-500; /* This line triggers a warning during build */
+	}
+</style>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
-
+	darkMode: ['selector', '[data-color-theme="dark"]'],
   theme: {
     extend: {}
   },
