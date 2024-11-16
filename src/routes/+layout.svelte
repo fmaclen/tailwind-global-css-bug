d8ba633ba3f9d5c884930d88913d87bc4a0355a4
Fernando Maclen <fmaclen@gmail.com>
2024-11-16 16:03:24 -0500

    fix: add workaround

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+  import "../app.css";
+  let { children } = $props();
 </script>
 
 {@render children()}
 
 <div class="markdown">
-	{@html `<pre>
-		<code>Background should be blue, not red</code>
-	</pre>`}
+  {@html `<div>
+		<code>All background colors are expected to be blue, red should not be visible</code>
+	</div>`}
 </div>
 
 <style lang="postcss">
-	.markdown :global(pre) {
-		@apply bg-red-500;
-		@apply dark:bg-blue-500; /* This line triggers a warning during build */
-	}
+  .markdown {
+    :global(div) {
+      @apply bg-red-500 p-10;
+
+      /* This line genreates incorrect CSS which breaks styling and throws warnings during build */
+      @apply dark:bg-blue-500;
+    }
+
+    /* WORKAROUND: These styles work as expected */
+    :global(code:where([data-color-theme="dark"], [data-color-theme="dark"] *)) {
+      @apply bg-blue-500;
+    }
+  }
 </style>
